8239e3e066162457a0300e04f7922c3ef00c1f08
Lothar Schulz <lothar.sch@gmx.de>
2018-12-05 21:21:31 +0100

    PODINFO_UI_COLOR set to green

--- demo.sh
+++ demo.sh
@@ -4,20 +4,20 @@ set -euo pipefail
 IFS=$'\n\t'
 
 clear
-printf  "change PODINFO_UI_COLOR to green\n\n\n"
+echo -e "change PODINFO_UI_COLOR to green\n\n\n"
 sleep 3
 sed -i -e 's/value: white/value: green/g' ./workloads/podinfo-dep.yaml
 sleep 1
 git commit -am "PODINFO_UI_COLOR set to green"
 git push origin master
-printf "PODINFO_UI_COLOR set to green.\n"
-printf "Deployment description should reflect that in a couple of seconds.\n"
-printf "--------------------------------\n\n\n"
+echo -e "PODINFO_UI_COLOR set to green.\n"
+echo -e "Deployment description should reflect that in a couple of seconds.\n"
+echo -e "--------------------------------\n\n\n"
 
 sleep 50
 
-printf "Clean up - set to original state.\n"
+echo -e "Clean up - set to original state.\n"
 sed -i -e 's/value: green/value: white/g' ./workloads/podinfo-dep.yaml
 git commit -am "PODINFO_UI_COLOR reset to white"
 git push origin master
-printf "Clean up done.\n\n\n"
+echo -e "Clean up done.\n\n\n"
